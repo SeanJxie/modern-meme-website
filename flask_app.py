74f15bcb4fe653172d23c5c109951286a7d3ee26
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2022-01-12 21:33:47 -0500

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, jsonify
 
 import threading
+import time
 
 import meme_gen
+
+
 
 app = Flask(__name__)
 
@@ -11,13 +14,12 @@ image_cache = []
 threads = []
 N_THREADS = 2
 
-
 def loader():
     while 1:
         image_cache.append(meme_gen.generate())
         print("Cache size:", len(image_cache))
 
-def load_thread():
+def load_threads():
     for _ in range(N_THREADS):
         th = threading.Thread(target=loader, daemon=True)
         threads.append(th)
@@ -25,28 +27,27 @@ def load_thread():
 
     print(f"Filled threads with {len(threads)} threads.")
 
-app_start = False
 
 @app.route('/')
 def index():
-    global app_start
-    print(app_start)
-    if not app_start:
-        print("start load")
-        load_thread()
-        app_start = True
-
+    print("Loading threads...")
+    load_threads()
+    print("Threads loaded.")
     return render_template("index.html")
+
 
 @app.route('/new_meme')
 def post_new_meme():
+    start = time.time()
+    print("Loading image...")
     if len(image_cache) > 0: 
         new_img_tag = meme_gen.serve_pil_image(image_cache[0])
         image_cache.pop(0)
     else:
-        new_img_tag = 'Meme'
+        new_img_tag = "Uh oh, you've ran out of memes! Please wait for more to generate and try again :)"
         
     data = {'meme': new_img_tag}
+    print(f"Image load took {time.time() - start}s")
     return jsonify(data)
 
 if __name__ == "__main__":
