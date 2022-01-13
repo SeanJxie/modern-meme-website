bcab82385e47a0b3ec07911bda2203fe013dc471
ray1i <mr251011@gmail.com>
2022-01-12 19:23:45 -0500

    no refresh implemented

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import meme_gen
 
 app = Flask(__name__)
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def index():
     new_img_tag = meme_gen.serve_pil_image(meme_gen.generate())
     return render_template("index.html", image=new_img_tag)
+
+@app.route('/new_meme')
+def post_new_meme():
+    new_img_tag = meme_gen.serve_pil_image(meme_gen.generate())
+    data = {'meme': new_img_tag}
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
